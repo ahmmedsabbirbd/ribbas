b55cd22a168e26efe6a5969c8ef36eca3d828f18
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-03-06 00:36:48 +0600

    Header done And Change&.02

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 $('documnet').ready(function() {
 
-  new WOW().init(); /*/// Wow start ///*/
+  new WOW().init(); /*wow start*/ 
   
+    $('.navbar-toggler').sidr({
+        side: 'right',
+        renaming: false,
+        displace: false,
+        source: '#asidr',
+        name: 'asidr-main',
+    });/*sidr-option*/
+
+    $('.sidr-close').on('click', function(){
+        $.sidr('close', 'asidr-main');
+    });/*sidr-open*/
+
+   /* 
+   $('body').swipe( {
+        swipeRight: function () {
+            $.sidr('close', 'asidr-main');
+            $('.show-overly').removeClass('active');
+        },
+        swipeLeft: function () {
+            $.sidr('open', 'asidr-main');
+            $('.show-overly').addClass('active')
+        }, 
+        threshold: 45
+    }); 
+    */
+    $('.navbar-toggler').on('click',function(){
+        $('.show-overly').addClass('active');
+    });/*sidr-overly-open*/
+
+    $('.search-triger').on('click',function(){
+        $('.search-bar, .show-overly').addClass('active');
+    });/*search-&-overly-open*/
+
+    $('.search-close, .sidr-close').on('click',function(){
+        $('.search-bar, .show-overly').removeClass('active');
+    });/*search-&-sidr-&-overly-close*/
+
 });
